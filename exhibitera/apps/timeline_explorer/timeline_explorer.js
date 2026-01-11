889c86bf9f3517a39b9f707d55e17e9a63c1703c
Morgan Rehnberg <forceflow1049@gmail.com>
2026-01-11 14:46:23 -0600

    Timeline scrolls horizontally in landscape mode

--- exhibitera/apps/timeline_explorer/timeline_explorer.js
+++ exhibitera/apps/timeline_explorer/timeline_explorer.js
@@ -230,15 +230,15 @@ function createTimelineEntry (itemUUID, langCode) {
 }
 
 // check if an element is in viewport
-// http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
 function isElementInViewport (el) {
   const rect = el.getBoundingClientRect()
   return (
-    rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    // Horizontal Check
+    rect.right >= 0 &&
+    rect.left <= (window.innerWidth || document.documentElement.clientWidth) &&
+    // Vertical Check
+    rect.bottom >= 0 &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight)
   )
 }
 
