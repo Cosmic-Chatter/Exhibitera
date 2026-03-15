a45a6f5dd88abf4cb63b4e6d1593df975a949a80
Morgan Rehnberg <forceflow1049@gmail.com>
2026-03-15 17:38:43 -0500

    Option for Core API library to write to Hub or Apps

--- exhibitera/apps/js/exhibitera_core_api.js
+++ exhibitera/apps/js/exhibitera_core_api.js
@@ -11,29 +11,39 @@ export function setInteraction (currentlyActive = true) {
   window.parent.exCommon.config.currentInteraction = currentlyActive
 }
 
-export function writeRawText (name, text, mode = 'a') {
-  // Send text to Exhibitera Apps to be saved to disk
+export function writeRawText (name, text, mode = 'a', local = true) {
+  // Send text to Exhibitera to be saved to disk
   // `name` should be the filename to create with no extention
   // `text` should be the raw text to be written
   // `mode` of 'a' means append to the existing text, while 'w' means overwrite
+  // If `local` is false, send the data to Hub if it is in use (this allows data
+  // to be aggregated between multiple app instances)
 
-  window.parent.exCommon.makeHelperRequest({
+  const options = {
     method: 'POST',
     endpoint: '/data/' + name + '/rawText',
     api: '/core',
     params: { mode, text }
-  })
+  }
+  if (window.parent.exCommon.standalone || local === true) {
+    window.parent.exCommon.makeHelperRequest(options)
+  } else window.parent.exCommon.makeServerRequest(options)
 }
 
-export async function getRawText (name) {
+export async function getRawText (name, local = true) {
   // Retrieve raw text stored by Exhibitera Apps
   // `name` should the a filename previously set by writeRawText()
 
-  const result = await window.parent.exCommon.makeHelperRequest({
+  const options = {
     method: 'GET',
     endpoint: '/data/' + name + '/rawText',
     api: '/core'
-  })
+  }
+  let result
+
+  if (window.parent.exCommon.standalone || local === true) {
+    result = await window.parent.exCommon.makeHelperRequest(options)
+  } else result = await window.parent.exCommon.makeServerRequest(options)
 
   if (result?.success) return result.text
 
